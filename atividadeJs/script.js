55e6e203755f869aee8c7b4ebcf306d03a4f0882
Grazy222 <graciele222@gmail.com>
2023-01-26 17:18:29 -0300

    desafio feito

---
+++ atividadeJs/script.js
@@ -0,0 +1,19 @@
+let food = "comida"
+    
+    switch (food) {
+    case lasanha:
+        console.log("Não temos essa comida aqui")
+        break;
+              
+    case pizza:
+        console.log("Estamos com escassez de pizza")
+        break;
+       
+        
+    case salada:
+        console.log("Aqui está, são 3 reais")
+        break;
+    default:
+        console.log("invalido")
+
+   }
